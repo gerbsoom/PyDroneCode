cb87d02bf8dc376a12a115c08948babdd07d1354
gerbsoom <gerbsoom>
2016-07-30 21:23:10 +0200

    Add first files to test out new directory structure.

---
+++ RemoteControl/launcher_RC.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# 
+# This file is part of PyDroneCode.
+# Please check the file LICENSE.MD for information about the license. 
+#
+# @file
+# @version 0.1
+# @copyright 2016 Desmodul
+# @author Markus Riegert <desmodul@drow-land.de>
+
+# ________________________________________________________________
+# Launches an interface to remote control a drone over the network
+
+import pygame
+from pygame.locals import *
+pygame.init()
+gameClock = pygame.time.Clock()
+
+from LoggerFactory import LogHandler
+logger = LogHandler().getLogger("Launcher")
+
+# setup network server
+# --> listen to port 21023 for remote control initialization
+# --> query initial sensor state in a non-emergency scenario
+
+#from GamepadController import GamepadController
+#gamepadController = GamepadController(pygame)
+# connect network/visualizer callbacks or use event emitting
+
+# try/catch or check if X available
+#from Visualizer import View3D
+#view3d = View3D(pygame)
+
+running = True
+while running:
+
+    pygame.event.pump()
+    # check for keyboard events, maybe allow setting commands from STD_IN
+	keys = pygame.key.get_pressed()
+    if keys[K_ESCAPE]:
+        running = False
+	
+	# cycle gamepad for button events or relevant throttle value changes
+	#gamepadController.doCycle()
+
+    # get the current orientation over network api and update view with it
+	#gyro = sensorSenseHat.getOrientationDegrees()
+    #simulator3D.rotateCubeToDegrees(360.0 - gyro["pitch"],
+    #                                360.0 - gyro["yaw"],
+    #                                360.0 - gyro["roll"])
+    gameClock.tick(50)
---
+++ SharedLibraries/LoggerFactory/LogHandler.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# 
+# This file is part of PyDroneCode.
+# Please check the file LICENSE.MD for information about the license. 
+#
+# @file
+# @version 0.3
+# @copyright 2016 Desmodul
+# @author Markus Riegert <desmodul@drow-land.de>
+# _______________________________________________________________
+# Offers basic functionalities to retrieve suitable loggers from
+# a valid configuration file, provided as constructor parameters.
+# Without config file logging falls back to print debug on STDout.
+
+import os, sys
+import os.path
+import logging
+import logging.config
+
+
+class LogHandler(object):
+    """ Tries to read the log configuration from a provided config file
+        and defaults otherwise to a simple STDout logging of all levels. """
+
+    def __init__(self, _logConfig="_log.conf"):
+
+        self.defaultFormat = "%(asctime)s [%(levelname)s] %(name)s %(message)s"
+
+        logOk = False
+        if os.path.isfile(_logConfig):
+            if os.access(_logConfig, os.R_OK):
+                try:
+                    logging.config.fileConfig(_logConfig)
+                    logOk = True
+                except:
+                    print(("ERROR: Parsing config!!! E=", sys.exc_info()[0]))
+            else:
+                print("ERROR: Config exists but is not accessible!!!")
+        else:
+            print("ERROR: Config does NOT exist!!!")
+
+        if logOk:
+            logging.getLogger(__name__).info("Config parsed successfully.")
+        else:
+            logging.basicConfig(format=self.defaultFormat, level='DEBUG')
+            logging.warn("Falling back to default log on STDout")
+
+    def getLogger(self, _name=""):
+        """ Returns a specific log instance the caller's name as channel. """
+        return logging.getLogger(_name)
