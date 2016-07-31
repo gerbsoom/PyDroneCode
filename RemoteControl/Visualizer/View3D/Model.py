ef85750201d4361f0ecfdb033468f46839addd71
gerbsoom <gerbsoom>
2016-07-31 16:49:20 +0200

    Fight back further modul import issues #8.

--- RemoteControl/Visualizer/View3D/Model.py
+++ RemoteControl/Visualizer/View3D/Model.py
@@ -20,12 +20,12 @@ import Point
 import Surface
 
 
-class Model():
+class Model(object):
     """ Represents the complete szene graph containing all created geometrical
         1/2/3D-Objects and offers some basic object building methods. """
 
     def __init__(self, _drone=[], _objects=[]):
-        super(Box, self).__init__(_objects)
+        super(Model, self).__init__(_drone, _objects)
 
         self.logger = LogHandler.getLogger(__name__)
         self.drone = _drone
