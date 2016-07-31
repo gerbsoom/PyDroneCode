4d73ded7f2e99c45e70a8f569e05f2a8c22ff72e
gerbsoom <gerbsoom>
2016-07-31 16:06:39 +0200

    Fix broken __init__py file.

--- RemoteControl/Visualizer/View3D/__init__.py
+++
@@ -1,7 +0,0 @@
-from Box import Box
-from Model import Model
-from Point import Point
-from Simulator import Simulator
-from Simulator import create
-from Surface import Surface
-from ViewPort import ViewPort
