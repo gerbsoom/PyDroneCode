52dbc3707eefc9a6931a0a08cd0b11a18bc43c2d
gerbsoom <gerbsoom>
2016-07-31 16:09:29 +0200

    Fight back further modul import issues #1.

--- RemoteControl/Visualizer/View3D/Simulator.py
+++ RemoteControl/Visualizer/View3D/Simulator.py
@@ -15,8 +15,8 @@
 from LoggerFactory import LogHandler
 from math import *
 
-import Model
-import ViewPort
+import Visualizer.View3D.Model as Model
+import Visualizer.View3D.ViewPort as ViewPort
 
 
 class Simulator(object):
