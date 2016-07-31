3d31273f2ac8b67518afaa748759f3c7487530e7
gerbsoom <gerbsoom>
2016-07-31 17:11:03 +0200

    fix typo

--- RemoteControl/Visualizer/View3D/Model.py
+++ RemoteControl/Visualizer/View3D/Model.py
@@ -25,7 +25,6 @@ class Model():
         1/2/3D-Objects and offers some basic object building methods. """
 
     def __init__(self, _drone=[], _objects=[]):
-        super(Box, self).__init__(_objects)
 
         self.logger = LogHandler.getLogger(__name__)
         self.drone = _drone
