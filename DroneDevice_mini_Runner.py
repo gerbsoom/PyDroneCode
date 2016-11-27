3851faaec17f7345ea447545352a78a231e9bd61
gerbsoom@web.de <gerbsoom>
2016-11-27 19:15:29 +0100

    Refactor ConfigConverter to ConfigLoader
    Refactor ConfigConverter to ConfigLoader

--- DroneDevice_mini_Runner.py
+++ DroneDevice_mini_Runner.py
@@ -10,21 +10,20 @@
 # @author Markus Riegert <desmodul@drow-land.de>
 #
 # ________________________________________________________________________________
-# Starter for the DroneDevice Application configured in: config/<application>.conf
+# Starter for the DroneDevice App configured in: config/<application>.conf
 #
 # Runner ID
 application = "drone-device_mini"
 
+import os
 import sys
-import configparser
-# import ConfigParser
-
-from Application import DroneDeviceApp
-from Application import DroneDeviceConf
-
 # append libs to module loading path
 sys.path.insert(0, "launcher/")
 sys.path.insert(0, "lib/")
+
+from Application import DroneDeviceApp
+#import ConfigParser
+import configparser
 
 # setup the logging from a config
 from LoggerFactory import LogHandler
@@ -36,8 +35,6 @@ logger.debug("PyRunner Location: " + str(sys.argv[0]))
 logger.debug("Working Directory: " + str(os.getcwd()))
 logger.debug("Library Module Path: " + str(sys.path))
 
-droneDeviceA
- 
-  
+
 if __name__ == '__main__':
     main()
